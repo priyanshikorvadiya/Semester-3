a7572d74bb62acdaadd05375cd2ecf60ce6a6c13
priyanshikorvadiya <priyanshi.korvadiya@gmail.com>
2026-06-29 22:05:52 +0530

    Add files via upload

---
+++ Data Structure/Lab 7/NodeStructure.c
@@ -0,0 +1,25 @@
+//Write a program to implement a node structure for singly linked list. Read the 
+//data in a node, print the node. 
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int info;
+    struct Node* link;
+};
+
+void main(){
+
+    struct Node* node = (struct Node*)malloc(sizeof  (struct Node*));
+
+    printf("Enter data of node:");
+    scanf("%d",node -> info);
+
+    node -> link = NULL;
+
+    printf("Node data : %d",node -> info);
+
+    free(node);
+}
