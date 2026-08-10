9bdddd69f21975601f49ef82585f894395415e92
Priyanshi Korwadiya <priyanshi.korvadiya@gmail.com>
2026-08-10 11:09:50 +0530

    Add files via upload

---
+++ Frontend Web Technology/Lab 15/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+    <p>&copy; 2026 My Website. All rights reserved.</p>
+    <nav>
+        <a href="#privacy">Privacy Policy</a> | 
+        <a href="#terms">Terms of Service</a>
+    </nav>
+    </footer>
+  )
+}
+
+export default Footer
