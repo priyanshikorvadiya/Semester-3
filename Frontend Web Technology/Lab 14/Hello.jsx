08f4000b6f054ac9a4b499ee7cd1d20ef9405c20
Priyanshi Korwadiya <priyanshi.korvadiya@gmail.com>
2026-08-10 11:08:45 +0530

    Add files via upload

---
+++ Frontend Web Technology/Lab 14/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <>
+    <h2>Hello From Function</h2>
+    </>
+  )
+}
+
+export default Hello
---
+++ Frontend Web Technology/Lab 14/HelloClass.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HelloClass extends Component {
+  render() {
+    return (
+      <>
+      <h1>Hello guys! Welcome.</h1>
+      </>
+    )
+  }
+}
