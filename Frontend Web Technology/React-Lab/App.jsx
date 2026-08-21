749f9e034bbe5416d41abf8b576e198baecaac7a
Priyanshi Korwadiya <priyanshi.korvadiya@gmail.com>
2026-08-21 19:57:16 +0530

    Add files via upload

---
+++ Frontend Web Technology/React-Lab/App.jsx
@@ -0,0 +1,31 @@
+import Header from './Lab 15/Header'
+import Footer from './Lab 15/Footer'
+import Props from './Lab 16/Props'
+import Events from './Lab 16/Events'
+import DisplayElement from './Lab 16/IsDisplay'
+import Faculties from './Lab 17/Faculties'
+import Products from './Lab 17/Products'
+import Students from './Lab 17/Students'
+
+
+function App() {
+ 
+  return (
+    <>
+    {/* <Header/>
+    <Footer/> */}
+
+    {/* <Props name='Rohit' age='37'/>
+    <Events/>
+    <DisplayElement/> */}
+
+    <Faculties/>
+    <Products/>
+    <Students/>
+
+    
+    </>
+  )
+}
+
+export default App
